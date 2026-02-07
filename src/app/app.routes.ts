978319fd6122e60acd0a837566723635a564eabd
Hend Adel <hendadel1012@gmail.com>
2026-02-08 00:36:33 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Blogs } from './components/blogs/blogs';
+import { NotFound } from './components/not-found/not-found';
+import { BlogDetails } from './components/blog-details/blog-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+  path:"",redirectTo:"home", pathMatch:"full"
+},
+{
+  path:"home", component:Home
+},
+{
+  path:"blogs", component:Blogs
+},
+{
+  path:"blog-details/:id", component:BlogDetails
+},
+
+
+
+
+
+
+
+
+{
+  path:"**", component:NotFound
+}
+
+]
